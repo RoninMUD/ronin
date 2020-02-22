7456a56ca93d3939459569968000475aa7278274
Night <night@shadris.net>
2020-02-21 20:16:16 -0800

    Fix list_vaults, as previous fix didn't work. Updated the program to be a little more sane.
    Update Makefile and roninstart script to build and copy list_vaults program as needed.

--- src/list_vaults.c
+++ src/list_vaults.c
@@ -1,72 +1,61 @@
 /*
-**  File: list_vaults.c, Special module.                   Part of DIKUMUD
+**  File: list_vaults.c                                     Part of DIKUMUD
 **  Will list the vaults a player has access to without causing machine lag.
 */
 
-/*
-$Author: ronin $
-$Date: 2004/02/05 16:09:36 $
-$Header: /home/ronin/cvs/ronin/list_vaults.c,v 2.0.0.1 2004/02/05 16:09:36 ronin Exp $
-$Id: list_vaults.c,v 2.0.0.1 2004/02/05 16:09:36 ronin Exp $
-$Name:  $
-$Log: list_vaults.c,v $
-Revision 2.0.0.1  2004/02/05 16:09:36  ronin
-Reinitialization of cvs archives
-
-Revision 1.2  2002/03/31 07:42:15  ronin
-Addition of header lines.
-
-$State: Exp $
-*/
-
-
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define UPPER(c) (((c)>='a'  && (c) <= 'z') ? ((c)+('A'-'a')) : (c) )
-#define CAP(st)  (*(st) = UPPER(*(st)), st)
+#define MAX_VAULTS 50
+
+#define UPPER(c)    (((c) >= 'a' && (c) <= 'z') ? ((c) + ('A' - 'a')) : (c))
+#define CAP(string) (*(string) = UPPER(*(string)), string)
 
 int main(int argc, char **argv) {
-  char name[255],buf[4096];
+  char name[255], buf[4096];
   FILE *fd;
-  int loop;
 
   if (!argv[1]) {
     printf("Unable to list access to vaults.\n\r");
+
     exit(0);
   }
-  sprintf (name,"%s",argv[1]);
 
-  remove("access.chk");
-  sprintf(buf,"grep -lx %s vault/*.name > vault/access.chk",name);
+  snprintf(name, sizeof(name), "%s", argv[1]);
+
+  remove("vault/access.list");
+
+  snprintf(buf, sizeof(buf), "grep -lx %s vault/*.name > vault/access.list", name);
+
   system(buf);
 
   printf("You have access to the following vaults...\n\r\n\r");
 
-  /* Check for their own vault */
-  sprintf(buf,"vault/%s.vault",name);
-  if((fd=fopen(buf,"r"))) {
-    printf("%s\n\r",CAP(name));
+  snprintf(buf, sizeof(buf), "vault/%s.vault", name);
+
+  if ((fd = fopen(buf, "r"))) {
+    printf("%s\n\r", CAP(name));
+
     fclose(fd);
   }
 
-  if(!(fd=fopen("vault/access.chk","r"))) {
+  if (!(fd = fopen("vault/access.list", "r"))) {
     printf("Unable to list access to further vaults.\n\r");
+
     exit(0);
   }
 
-  loop=0;
-  *name = '\0';
-  while(!feof(fd) && loop<30) {
-    fscanf(fd, "vault/%[a-z].name\n",name);
-    if(*name) {
-      printf("%s\n\r",CAP(name));
+  memset(name, 0, sizeof(name));
+
+  for (int i = 0; !feof(fd) && (i < MAX_VAULTS); i++) {
+    if (fscanf(fd, "vault/%[a-z].name\n", name)) {
+      printf("%s\n\r", CAP(name));
     }
-    loop++;
   }
+
   fclose(fd);
+
   exit(0);
 }
-
