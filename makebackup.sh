ef1a43ea09da5a1a11aad68b48e154db6e3856ce
Night <night@shadris.net>
2021-11-16 22:47:39 -0800

    More script tweaks.

--- makebackup.sh
+++ makebackup.sh
@@ -3,7 +3,7 @@ TYPE=$(basename $PWD)
 PREFIX="../backup/$TYPE/$(date +%Y%m%d)-$TYPE"
 BASEDIR="."
 EXT="tar.xz"
-TAROPT="Jcvf"
+TAROPT="cfJv"
 
 if [ -f $PREFIX-base.$EXT ]
 then
@@ -11,48 +11,36 @@ then
 else
   tar $TAROPT $PREFIX-base.$EXT \
    --owner=ronin --group=ronin \
-   --exclude=.svn \
    --exclude=.git \
    --exclude='*.o' \
    --exclude="$BASEDIR/syslogs/*" \
    --exclude="$BASEDIR/runlog" \
    --exclude="$BASEDIR/syslog" \
    --exclude="$BASEDIR/syslog.last" \
-   --exclude="$BASEDIR/lib/plrlog*" \
-   --exclude="$BASEDIR/lib/death.log" \
-   --exclude="$BASEDIR/lib/warnings.log" \
-   --exclude="$BASEDIR/lib/mail/*" \
-   --exclude="$BASEDIR/lib/post/*" \
-   --exclude="$BASEDIR/lib/PURGED/*" \
-   --exclude="$BASEDIR/lib/PURGENEWBIE/*" \
-   --exclude="$BASEDIR/lib/rent/?/*.*" \
-   --exclude="$BASEDIR/lib/rent/*.list" \
-   --exclude="$BASEDIR/lib/rent/*.stat" \
-   --exclude="$BASEDIR/lib/rent/death/*" \
-   --exclude="$BASEDIR/lib/TPURGE/*" \
-   --exclude="$BASEDIR/lib/VAULTBACKUP/*" \
-   --exclude="$BASEDIR/lib/vault/*" \
-   --exclude="$BASEDIR/lib/VPURGED/*" \
+   --exclude="$BASEDIR/bin/ronin" \
+   --exclude="$BASEDIR/bin/roninslave" \
    --exclude="$BASEDIR/lib/*.messages" \
-   --exclude="$BASEDIR/lib/bugs.old.Z" \
+   --exclude="$BASEDIR/lib/auction.data" \
    --exclude="$BASEDIR/lib/clanlist.*" \
    --exclude="$BASEDIR/lib/clans" \
+   --exclude="$BASEDIR/lib/death.log" \
    --exclude="$BASEDIR/lib/highest_id*" \
-   --exclude="$BASEDIR/lib/auction.data" \
    --exclude="$BASEDIR/lib/lottery.data" \
+   --exclude="$BASEDIR/lib/plrlog" \
+   --exclude="$BASEDIR/lib/mail/*" \
+   --exclude="$BASEDIR/lib/post/*" \
+   --exclude="$BASEDIR/lib/rent/?/*.*" \
+   --exclude="$BASEDIR/lib/rent/death/*" \
+   --exclude="$BASEDIR/lib/vault/*" \
    --exclude="$BASEDIR/lib/names-*" \
    --exclude="$BASEDIR/lib/player_idname*" \
    --exclude="$BASEDIR/lib/rank_file*.dat" \
-   --exclude="$BASEDIR/lib/wizlist*" \
    --exclude="$BASEDIR/lib/slave.pid" \
-   --exclude="$BASEDIR/lib/last_command.txt" \
-   --exclude="$BASEDIR/lib/rent/eq-allfiles" \
+   --exclude="$BASEDIR/lib/vote*.dat" \
+   --exclude="$BASEDIR/lib/warnings.log" \
+   --exclude="$BASEDIR/lib/wizlist*" \
    --exclude="$BASEDIR/obj/*" \
-   --exclude="$BASEDIR/src/nlp" \
-   --exclude="$BASEDIR/src/libdmalloc.a" \
-   --exclude=bin/ronin \
-   --exclude=bin/roninslave \
-   $BASEDIR/ bin/
+   $BASEDIR/
 fi
 
 if [ -f $PREFIX-data.$EXT ]
@@ -61,33 +49,26 @@ then
 else
   tar $TAROPT $PREFIX-data.$EXT \
    --owner=ronin --group=ronin \
-   --exclude=.svn \
    --exclude=.git \
    --exclude="*.o" \
    --exclude="$BASEDIR/lib/vault/vault.log" \
-   --exclude="$BASEDIR/lib/vault/vault-allfiles" \
    $BASEDIR/lib/mail/ \
    $BASEDIR/lib/post/ \
-   $BASEDIR/lib/PURGED/ \
-   $BASEDIR/lib/PURGENEWBIE/ \
    $BASEDIR/lib/rent/?/ \
-   $BASEDIR/lib/rent/*.list \
-   $BASEDIR/lib/rent/*.stat \
    $BASEDIR/lib/rent/death/ \
-   $BASEDIR/lib/TPURGE/ \
-   $BASEDIR/lib/VAULTBACKUP/ \
    $BASEDIR/lib/vault/ \
-   $BASEDIR/lib/VPURGED/ \
    $BASEDIR/lib/*.messages \
-   $BASEDIR/lib/bugs.old.Z \
+   $BASEDIR/lib/clanlist.* \
+   $BASEDIR/lib/*.messages \
+   $BASEDIR/lib/auction.data \
    $BASEDIR/lib/clanlist.* \
    $BASEDIR/lib/clans \
    $BASEDIR/lib/highest_id* \
-   $BASEDIR/lib/auction.data \
    $BASEDIR/lib/lottery.data \
    $BASEDIR/lib/names-* \
    $BASEDIR/lib/player_idname* \
    $BASEDIR/lib/rank_file*.dat \
+   $BASEDIR/lib/vote*.dat \
    $BASEDIR/lib/wizlist*
 fi
 
@@ -97,15 +78,14 @@ then
 else
   tar $TAROPT $PREFIX-log.$EXT \
    --owner=ronin --group=ronin \
-   --exclude=.svn \
    --exclude=.git \
    --exclude="*.o" \
    $BASEDIR/syslogs/ \
    $BASEDIR/runlog \
    $BASEDIR/syslog \
    $BASEDIR/syslog.last \
-   $BASEDIR/lib/plrlog* \
    $BASEDIR/lib/death.log \
+   $BASEDIR/lib/plrlog \
    $BASEDIR/lib/warnings.log \
    $BASEDIR/lib/vault/vault.log
 fi
