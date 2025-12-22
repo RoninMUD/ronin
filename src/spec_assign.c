a81583d914955a582f4c8b0bfd6efb5a8e49fd39
Aaron Grimmer <87430970+Arodtanjoe@users.noreply.github.com>
2025-12-21 22:13:25 -0600

    Add files via upload
    Updated Build Files for new zone Specs
    
    Signed-off-by: Aaron Grimmer <87430970+Arodtanjoe@users.noreply.github.com>

--- src/spec_assign.c
+++ src/spec_assign.c
@@ -125,6 +125,7 @@ void assign_WOT(void);
 void assign_ecanyon(void);
 void assign_turkeytakeover(void);
 void assign_medievalblackmarket(void);
+void assign_christmasvillage(void);
 void assign_deathplaygroundancientgrave(void);
 
 //void assign_wbw(void);
@@ -142,7 +143,6 @@ void assign_darkspire(void);
 void assign_morgoth(void);
 void assign_olympus(void);
 void assign_deathplaygroundcrypt(void);
-void assign_christmasvillage(void);
 #endif
 
 void assign_mobiles(void)
@@ -229,7 +229,8 @@ void assign_mobiles(void)
   assign_WOT();
   assign_ecanyon();
   assign_turkeytakeover();
-  assign_medievalblackmarket();
+  assign_medievalblackmarket();  
+  assign_christmasvillage();
   assign_deathplaygroundancientgrave();
 //  assign_wbw();
 #ifdef TEST_SITE
@@ -246,7 +247,6 @@ void assign_mobiles(void)
   assign_morgoth();
   assign_olympus();
   assign_deathplaygroundcrypt();
-  assign_christmasvillage();
 #endif
 }
 
